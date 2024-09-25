5c5760c6c20d569f7794bd146d33cd1d04517245
Sth-I-T-Way <163323939+Sth-I-T-Way@users.noreply.github.com>
2024-09-25 21:27:42 +0300

    Добавлен INA226

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,48 @@
 #include <WiFi.h>
+#include <INA226.h>
 
+#define IDBAT 1  //id устр-ва
 const char* ssid = "A-415";
 const char* password = "LongRedAlert5";
 
 const uint16_t port = 8090;
 const char * host = "192.168.1.108";
 
+INA226 INA(0x40);
+
+
 void setup()
 {
-
   Serial.begin(115200);
-
   WiFi.begin(ssid, password);
+  
+  Wire.begin();
+  if (!INA.begin() )
+  {
+    Serial.println("it was not possible to connect to the voltammeter. Fix the error");
+  }
+  INA.setMaxCurrentShunt(1, 0.002);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("...");
   }
-
   Serial.print("WiFi connected with IP: ");
   Serial.println(WiFi.localIP());
-
 }
 
 void loop()
 {
     WiFiClient client;
-
     if (!client.connect(host, port)) {
-
         Serial.println("Connection to host failed");
-
-        delay(1000);
         return;
     }
+    Serial.println("The Wi-Fi connection is fine");
 
-    Serial.println("Connected to server successful!");
-
-    client.print("Hello from ESP32!");
-
-    Serial.println("Disconnecting...");
-    client.stop();
-
-    delay(10000);
+    //сообщение\/
+    client.print("{\"ID\":" + String(IDBAT)
+    + ", \"V\":" + String(INA.getBusVoltage())
+    + ", \"A\":" + String(INA.getCurrent_mA()) + "}");
+    delay(1000);
 }
