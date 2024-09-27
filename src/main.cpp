d68b5f0b6845d10544cf79c4cfd1f0cdc1749a76
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2024-09-28 00:10:32 +0300

    Фикс: исправлена безопасность и немного рефакторинга

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,55 @@
 #include <WiFi.h>
 #include <INA226.h>
 #include <ESPmDNS.h>
-//--------------НАСТРОЙКИ---------------
-#define IDBAT 1                         //id устр-ва
-const char* ssid = "A-415";             //WI-FI
-const char* password = "LongRedAlert5"; //Пароль от WI-FI
 
-const uint16_t port = 8090;             
-const char * hostname = "WORK-SHIWARAI";
-//---------------------------------------
+#define SERIALIZE_DATA(ID, V, A, P, C) "{\"ID\":" + ID + ", \"V\":" + V + ", \"A\":" + A + ", \"P\":" + P + ", \"C\":" + C + "}"
 
 IPAddress HOSTIP;
 
 INA226 INA(0x40);
 WiFiClient client;
+String data;
 
 void setup()
 {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  
-  Wire.begin();
-  if (!INA.begin() )
-  {Serial.println("it was not possible to connect to the voltampermeter. Fix the error");}
-  INA.setMaxCurrentShunt(1, 0.002);
-  
-  //подключение к WIFI
-  while (WiFi.status() != WL_CONNECTED) {delay(500);}
-  Serial.print("WiFi connected with IP: ");
-  Serial.println(WiFi.localIP());
+	Serial.begin(115200);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	
+	Wire.begin();
+	if (!INA.begin())
+		Serial.println("it was not possible to connect to the voltampermeter. Fix the error");
+	INA.setMaxCurrentShunt(60, 0.00125, true);
+	
+	// подключение к WIFI
+	while (WiFi.status() != WL_CONNECTED) {delay(500);}
+	Serial.print("WiFi connected with IP: ");
+	Serial.println(WiFi.localIP());
 
 
-  while(mdns_init()!= ESP_OK){delay(1000);}
+	while(mdns_init()!= ESP_OK){delay(1000);}
 
-//поиск ip адреса hostname
-  while (HOSTIP.toString() == "0.0.0.0") {
-  delay(250);
-  HOSTIP = MDNS.queryHost(hostname);
-  }
+	// поиск ip адреса hostname
+	while (HOSTIP.toString() == "0.0.0.0") {
+		delay(250);
+		HOSTIP = MDNS.queryHost(HOSTNAME);
+	}
 }
 
 void loop()
 {
-    //если потеряли связь с клиентом, то устонавливаем её заново
-    while(!client.connected()){client.connect(HOSTIP, port);delay(1000);}
+	delay(1000);
+	// если потеряли связь с клиентом, то устонавливаем её заново
+	if(!client.connected())
+	{
+		client.connect(HOSTIP, PORT);
+		return;
+	}
 
-    //   \/ сообщение по COM порту \/
-    Serial.println("{\"ID\":" + String(IDBAT)
-    + ", \"V\":" + String(INA.getBusVoltage())
-    + ", \"A\":" + String(INA.getCurrent_mA()) 
-    + ", \"Ch\":" + map(INA.getBusVoltage(),3.3,4.2,0,100) + "}");
+	data = SERIALIZE_DATA(String(ID), INA.getBusVoltage(), INA.getCurrent(), INA.getPower(), map(INA.getBusVoltage(),3.3,4.2,0,100));
 
-    //     \/ сообщение по WIFI \/
-    client.print("{\"ID\":" + String(IDBAT)
-    + ", \"V\":" + String(INA.getBusVoltage())
-    + ", \"A\":" + String(INA.getCurrent_mA()) 
-    + ", \"Ch\":" + map(INA.getBusVoltage(),3.3,4.2,0,100) + "}");
-    delay(1000);
+	//   \/ сообщение по COM порту \/
+	Serial.println(data);
+
+	//     \/ сообщение по WIFI \/
+	client.print(data);
 }
