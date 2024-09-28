d4f43d053c211350ccd3c9d6880c8b26f8b78dbf
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2024-09-28 19:00:16 +0300

    Фикс: исправлен уровень заряда

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 #include <ESPmDNS.h>
 
 #define SERIALIZE_DATA(ID, V, A, P, C) "{\"ID\":" + ID + ", \"V\":" + V + ", \"A\":" + A + ", \"P\":" + P + ", \"C\":" + C + "}"
+#define FLOAT_MAP(value, in_min, in_max, out_min, out_max) (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 IPAddress HOSTIP;
 
 INA226 INA(0x40);
 WiFiClient client;
 String data;
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void setup()
 {
@@ -18,7 +23,7 @@ void setup()
 	Wire.begin();
 	if (!INA.begin())
 		Serial.println("it was not possible to connect to the voltampermeter. Fix the error");
-	INA.setMaxCurrentShunt(60, 0.00125, true);
+	INA.setMaxCurrentShunt(60, 0.00125, false);
 	
 	// подключение к WIFI
 	while (WiFi.status() != WL_CONNECTED) {delay(500);}
@@ -45,7 +50,7 @@ void loop()
 		return;
 	}
 
-	data = SERIALIZE_DATA(String(ID), INA.getBusVoltage(), INA.getCurrent(), INA.getPower(), map(INA.getBusVoltage(),3.3,4.2,0,100));
+	data = SERIALIZE_DATA(String(ID), INA.getBusVoltage(), INA.getCurrent(), INA.getPower(), floatMap(INA.getBusVoltage(),3.3,4.2,0.0,100.0));
 
 	//   \/ сообщение по COM порту \/
 	Serial.println(data);
